7a70d65ad02c63f74bceae838df81eb3c6e7d106
keannekeannekeanne <karstenvarua@gmail.com>
2026-04-10 10:48:02 +0800

    Another trial

---
+++ HomeProjects/src/Adding.java
@@ -0,0 +1,9 @@
+public class Adding {
+    public static void main(String[] args){
+        int totalSum=0;
+        for (int i = 1; i <= 100; i++) {
+            totalSum += i;
+        }
+        System.out.println(totalSum);
+    }
+}
